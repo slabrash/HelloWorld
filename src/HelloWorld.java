b99f5c2c34c29758e902a5bc8596fcf07b472c34
slabrash <splabrash@gmail.com>
2016-12-25 08:31:49 -0800

    Changed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
     {
 
         System.out.println("Hello World!");
-        System.out.println("Hello There");
         System.out.println("Bye");
+        System.out.println("Hello again");
     }
 }
